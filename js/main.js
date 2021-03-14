7ebce0b202bf200860a8e0a95332b5d9b5003dbd
Shamil Omarov <s_omarov05@mail.ru>
2021-03-14 22:09:07 +0300

    Удалил cons.log из JS файла

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let hyperLinks = document.querySelectorAll('a');
 
 for(let i = 0; i < hyperLinks.length; i++) {
-    console.log(hyperLinks[i]);
     if(hyperLinks[i].getAttribute('href') === '#') {
         hyperLinks[i].style.color = 'red';
     }   

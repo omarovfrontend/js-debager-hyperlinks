e570b181c626927e8fae06f24c40174b952c7642
Shamil Omarov <s_omarov05@mail.ru>
2021-03-15 15:35:26 +0300

    Поправил ревью

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-let hyperLinks = document.querySelectorAll('a');
+// let hyperLinks = document.querySelectorAll('a');
 
-for(let i = 0; i < hyperLinks.length; i++) {
-    if(hyperLinks[i].getAttribute('href') === '#') {
-        hyperLinks[i].style.color = 'red';
-    }   
-}
+// for(let i = 0; i < hyperLinks.length; i++) {
+//     if(hyperLinks[i].getAttribute('href') === '#') {
+//         hyperLinks[i].style.color = 'red';
+//     }   
+// }
+
+// или можно так:
+document.querySelectorAll('a').forEach(link => {
+    if (link.getAttribute('href') === '#') {
+        link.style.color = 'red';
+    }
+})
